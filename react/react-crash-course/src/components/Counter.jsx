6885cafe7061585a29fc5f559e6ebbd22008895c
Ryan <Woodedcode@gmail.com>
2026-07-29 21:33:00 -0500

    Update Counter.jsx

--- react/react-crash-course/src/components/Counter.jsx
+++ react/react-crash-course/src/components/Counter.jsx
@@ -1,22 +1,32 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react'
 
 function Counter() {
-    const [counter, setCounter] = useState(0)
-    function incrementCounter() {
-        setCounter(counter + 1)
-    }
-    
-    function decrementCounter() {
-        setCounter(counter - 1)
+    const [cart,setCart] = useState({ 
+        item: 'apple', 
+        quantity: 0 
+    });
+
+    function removeApple() {
+        setCart((prevCart) => ({
+            ...prevCart,
+            quantity: prevCart.quantity - 1
+        }))
     }
 
+    function addApple() {
+        setCart((prevCart) => ({
+            ...prevCart,
+            quantity: prevCart.quantity + 1,
+        }))
+    }
     return (
         <div>
-            <button onClick={decrementCounter}>-</button>
-            {counter}
-            <button onClick={incrementCounter}>+</button>
+            <button onClick={removeApple}>-</button>
+            {cart.quantity}
+            {cart.item}
+            <button onClick={addApple}>+</button>
         </div>
     )
 }
 
-export default Counter
+export default Counter;
