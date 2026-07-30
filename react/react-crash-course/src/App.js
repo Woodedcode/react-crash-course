1cecf1babbaf74f751bfab51ab8688dbf3e26859
Ryan <Woodedcode@gmail.com>
2026-07-29 22:48:41 -0500

    Update App.js

--- react/react-crash-course/src/App.js
+++ react/react-crash-course/src/App.js
@@ -1,29 +1,55 @@
 import './App.css';
-import Todo from './components/Todo.jsx';
-import Title from './components/Title.jsx';
-import Modal from './components/Modal.jsx';
-import Counter from'./components/Counter.jsx'
-import React, { useState } from 'react';
+import Todo from './components/Todo.jsx'
+import Title from './components/Title.jsx'
+import Modal from './components/Modal.jsx'
+import React, { useState, useEffect } from 'react'
+// import Counter from './components/Counter.jsx'
 
 function App() {
-  return <Counter />
-
   const [showModal, setShowModal] = useState(false)
+
+  function onTodoDelete() {
+    setShowModal(true)
+  }
+
+  function cancelModal() {
+    setShowModal(false)
+  }
+
+  function confirmModal() {
+    setShowModal(false)
+  }
+
+  useEffect(() => {
+    console.log(`ONLY on mount AND on ${showModal} change`)
+  }, [showModal])
+
+  useEffect(() => {
+    console.log('on mount')
+  }, [showModal])
+
+  useEffect(() => {
+    console.log('EVERY render')
+  })
+
   return (
     <div className="App">
-      <Title />
+      <Title/>
       <div>
         <input type="text" onChange={(event) => {
           console.log(event.target.value)
         }} />
-        <button>Add todo</button>
+        <button onClick={() => setShowModal(true)}>Add Todo</button>
       </div>
       <div className="todo__wrapper">
-        <Todo title="Finish Frontend Simplified"/>
-        <Todo title="Finish Interview Section"/>
-        <Todo title="Land a $100k job"/>
+        <Todo onTodoDelete={onTodoDelete} title="Finish Frontend Simplified"/>
+        <Todo onTodoDelete={onTodoDelete}  title="Finish interview section"/>
+        <Todo onTodoDelete={onTodoDelete} title="land a $100k job"/>
       </div>
-    {showModal ? <Modal text="Confirm Delete" /> : null}
+      {showModal && <Modal 
+      cancelModal={cancelModal} 
+      confirmModal={confirmModal} 
+      confirm="Confirm Delete?"/>}
     </div>
   );
 }
